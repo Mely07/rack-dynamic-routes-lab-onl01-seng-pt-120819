f63164366deaa5661111348bf86947d4c2ff7df7
Damely Tineo <damely.tineo07@gmail.com>
2020-03-12 03:58:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       
       item = @@items.find{|x| x.name == item_name}
+      
       if item 
         resp.write item.price
       else 
