493083f69c62cdc089784b768f5d46830c3dc8be
Damely Tineo <damely.tineo07@gmail.com>
2020-03-12 03:17:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,17 @@
 class Application
  
+ @@item = []
+ 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path=="/items"
-      resp.write ""
+    if req.path=="/item"
+      
+      resp.write "You requested the songs"
     else
       resp.write "Route not found"
-      resp.status = 400
+      resp.status = 404
     end
  
     resp.finish
