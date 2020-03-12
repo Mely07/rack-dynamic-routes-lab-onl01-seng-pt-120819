d631820f7fae73974a80fc2adf2075b863388137
Damely Tineo <damely.tineo07@gmail.com>
2020-03-12 03:36:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path.match(/items/name)
+    if req.path.match(/items/:name)
       item = req.params["item"]
       if @@items.include?(item)
         resp.write "x"
