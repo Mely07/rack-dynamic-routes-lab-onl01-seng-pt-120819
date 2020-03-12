dc6d8a6599b92b43aff16de517dee8bf7ded15ac
Damely Tineo <damely.tineo07@gmail.com>
2020-03-12 03:38:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,25 +2,25 @@ class Application
  
  @@items = []
  
-def call(env)
-  resp = Rack::Response.new
-  req = Rack::Request.new(env)
-
-  if req.path.match(/items)
-    item = req.params["item"]
-    if @@items.include?(item)
-      resp.write "x"
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+  
+    if req.path.match(/items)
+      item = req.params["item"]
+      if @@items.include?(item)
+        resp.write "x"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
-      resp.write "Item not found"
-      resp.status = 400
+      resp.write "Route not found"
+      resp.status = 404
     end
-  else
-    resp.write "Route not found"
-    resp.status = 404
+  
+  
+    resp.finish
   end
-
-
-  resp.finish
-end
   
 end
