325298dd15da799f08ecaaaad902ec41167ef6f5
Damely Tineo <damely.tineo07@gmail.com>
2020-03-12 04:01:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,6 @@ class Application
   
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      /a/b/c/items/Fig
       item = @@items.find{|x| x.name == item_name}
       
       if item 
